c70b13b91921accd7a51afb8ec732f6044f15763
DI-AQUARIUS <aquarius@aquarius-air.local>
2022-01-21 17:33:21 +0900

    bufferというのでエラーが出てしまった。

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { buffer: false };
 
     return config;
   },
